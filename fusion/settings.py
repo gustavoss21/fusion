6a7386901574af8dc2833ab2b5b169407946cffb
santo <gustavosantos65@gmail.com>
2022-03-30 15:47:55 -0300

    projeto ajustes

--- fusion/settings.py
+++ fusion/settings.py
@@ -84,9 +84,8 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 #     }
 # }
 DATABASES = {
-     'default': {
-         dj_database_url.config()
-     }}
+     'default': {dj_database_url.config(),}
+            }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
